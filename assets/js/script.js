e125fbc52c72f2a4b367b08fc8116603a8854575
--Dmitriy <dpemvr@yandex.ru>
2023-01-15 17:41:24 +0500

    Add slick carousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,30 +144,6 @@ for (let item of btnsAdd) {
 
 
 
-
-// slider
-
-const swiper = new Swiper('.swiper', {
-     // Optional parameters
-     direction: 'horizontal',
-     loop: true,
-   
-     // If we need pagination
-     pagination: {
-       el: '.swiper-pagination',
-     },
-   
-     // Navigation arrows
-     navigation: {
-       nextEl: '.swiper-button-next',
-       prevEl: '.swiper-button-prev',
-     },
-   
-     // And if we need scrollbar
-     scrollbar: {
-       el: '.swiper-scrollbar',
-     },
-});
    
 
 
@@ -222,3 +198,9 @@ function initRatings() {
           };
      };
 };
+
+
+// slider-Action
+
+
+$('.single-item').slick();
